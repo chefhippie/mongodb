6a06e6f4471dbd68b91731a1ef06574d3bceb636
Thomas Boerger <tboerger@tbpro.de>
2013-12-21 16:48:37 +0100

    Initial commit

---
+++ attributes/default.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: mongodb
+# Attributes:: default
+#
+# Copyright 2013, Thomas Boerger
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+case node["platform_family"]
+when "debian"
+  default["mongodb"]["apt"]["uri"] = "http://downloads-distro.mongodb.org/repo/debian-sysvinit"
+  default["mongodb"]["apt"]["distribution"] = "dist"
+  default["mongodb"]["apt"]["components"] = %w(10gen)
+  default["mongodb"]["apt"]["keyserver"] = "keyserver.ubuntu.com"
+  default["mongodb"]["apt"]["key"] = "7F0CEB10"
+  default["mongodb"]["apt"]["source"] = false
+when "ubuntu"
+  default["mongodb"]["apt"]["uri"] = "http://downloads-distro.mongodb.org/repo/ubuntu-upstart"
+  default["mongodb"]["apt"]["distribution"] = "dist"
+  default["mongodb"]["apt"]["components"] = %w(10gen)
+  default["mongodb"]["apt"]["keyserver"] = "keyserver.ubuntu.com"
+  default["mongodb"]["apt"]["key"] = "7F0CEB10"
+  default["mongodb"]["apt"]["source"] = false
+end
